98d6ff677f329ca894d8e1a9f1d11c7e50a24523
Oleksandr Demchuk <alexfrostoleshyn@gmail.com>
2022-04-13 21:04:46 +0300

    add all files

---
+++ src/components/QuestionsList.js
@@ -0,0 +1,49 @@
+const QuestionsList = [
+  {
+    question: "What is the full form of WWW?",
+    answersList: [
+      {answer: "World Width Web", isCorrect: false},
+      {answer: "World Warr Won", isCorrect: false},
+      {answer: "World Wide Web", isCorrect: true},
+      {answer: "World Web Wide", isCorrect: false},
+    ],
+  },
+  {
+    question: "What is capotal of USA?",
+    answersList: [
+      {answer: "New York", isCorrect: false},
+      {answer: "Washington DC", isCorrect: true},
+      {answer: "Chicago", isCorrect: false},
+      {answer: "Florida", isCorrect: false},
+    ],
+  },
+  {
+    question: "What is the highest peak in the world?",
+    answersList: [
+      {answer: "Mount Everest", isCorrect: true},
+      {answer: "Mount K2", isCorrect: false},
+      {answer: "Annapurna", isCorrect: false},
+      {answer: "Makalu", isCorrect: false},
+    ],
+  },
+  {
+    question: "What is the highest earning Youtuber in 2021?",
+    answersList: [
+      {answer: "Jake Paul", isCorrect: false},
+      {answer: "Mr. Beast", isCorrect: true},
+      {answer: "Logan Paul", isCorrect: false},
+      {answer: "Dude Perfect", isCorrect: false},
+    ],
+  },
+  {
+    question: "Who maintains React?",
+    answersList: [
+      {answer: "Google", isCorrect: false},
+      {answer: "Amazon", isCorrect: false},
+      {answer: "Facebook", isCorrect: true},
+      {answer: "Apple", isCorrect: false},
+    ],
+  },
+];
+
+export default QuestionsList;
